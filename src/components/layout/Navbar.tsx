1d985679f5e8754de3a7f775a9709dc885426904
mohamedatef73 <matef4202@gmail.com>
2025-02-18 19:04:38 +0000

    add feature

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
 import { ShoppingCartIcon } from "@heroicons/react/24/outline";
+import { useCart } from "../../context/CartContext";
 
 const Navbar = () => {
+  const { cartItems } = useCart();
+  const itemCount = cartItems.reduce((total, item) => total + item.quantity, 0);
   return (
     <nav className="bg-white border-b border-gray-200 fixed w-full z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -23,8 +26,16 @@ const Navbar = () => {
             <Link to="/contact" className="text-gray-600 hover:text-gray-900">
               Contact Us
             </Link>
-            <Link to="/cart" className="text-gray-600 hover:text-gray-900">
+            <Link
+              to="/cart"
+              className="text-gray-600 hover:text-gray-900 relative"
+            >
               <ShoppingCartIcon className="h-6 w-6" />
+              {itemCount > 0 && (
+                <div className="absolute -top-2 -right-2 bg-red-500 text-white text-xs font-bold rounded-full h-5 w-5 flex items-center justify-center">
+                  {itemCount}
+                </div>
+              )}
             </Link>
           </div>
         </div>
