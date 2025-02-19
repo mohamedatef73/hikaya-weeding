af1ce91da021ee1740f8873ec4be61acbc440688
mohamedatef73 <matef4202@gmail.com>
2025-02-19 22:40:40 +0000

    edit

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
 import ProductDetails from "./pages/ProductDetails";
+import About from "./pages/About.tsx";
+import Contact from "./pages/Contact.tsx";
+import Categories from "./pages/Categories.tsx";
 import Navbar from "./components/layout/Navbar";
 import routes from "tempo-routes";
 
@@ -18,6 +21,9 @@ function App() {
           <Route path="/cart" element={<Cart />} />
           <Route path="/checkout" element={<Checkout />} />
           <Route path="/product/:id" element={<ProductDetails />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/categories" element={<Categories />} />
         </Routes>
         {import.meta.env.VITE_TEMPO === "true" && useRoutes(routes)}
         <FloatingChat />
